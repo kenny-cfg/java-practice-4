b600b9a02efdc19b33125b56d995aed5786d6142
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-12 18:43:55 +0100

    Small nicefication

--- java-practice-4/src/main/java/com/kenny/wenny/practice/CalculatorUtil.java
+++ java-practice-4/src/main/java/com/kenny/wenny/practice/CalculatorUtil.java
@@ -3,9 +3,6 @@ package com.kenny.wenny.practice;
 public class CalculatorUtil {
     // Primitive obsession!
     public static boolean equalsSum(int sum, int num1, int num2) {
-        if (num1 + num2 == sum) {
-            return true;
-        }
-        return false;
+        return num1 + num2 == sum;
     }
 }

7a4f0ff2c4200ad18c69024d13b27789149eec7c
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-12 18:40:26 +0100

    Solution to solo practice

---
+++ java-practice-4/src/main/java/com/kenny/wenny/CalculatorUtil.java
@@ -0,0 +1,11 @@
+package com.kenny.wenny;
+
+public class CalculatorUtil {
+    // Primitive obsession!
+    public static boolean equalsSum(int sum, int num1, int num2) {
+        if (num1 + num2 == sum) {
+            return true;
+        }
+        return false;
+    }
+}
--- java-practice-4/src/main/java/com/kenny/wenny/Main.java
+++ java-practice-4/src/main/java/com/kenny/wenny/Main.java
@@ -2,6 +2,19 @@ package com.kenny.wenny;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(CalculatorUtil.equalsSum(2, 1, 1));
+        System.out.println(CalculatorUtil.equalsSum(2, 3, 4));
     }
 }
+
+/*
+Create a class CalculatorUtil with a static method as follows, &
+implement the missing logic
+public static boolean equalsSum(int sum, int num1, int num2){
+// logic here
+// if num1 + num2 = sum, return true, otherwise false
+}
+●
+Use this static method in a main method in another class (e.g. a class
+called Main)
+ */
